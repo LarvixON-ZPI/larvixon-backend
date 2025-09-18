e1c881992b2f6f91490546ffed56788c5736b3d3
charmosaa <272682@student.pwr.edu.pl>
2025-09-18 11:55:51 +0200

    feat: create video analysis on uploading the video

--- videoprocessor/views/upload.py
+++ videoprocessor/views/upload.py
@@ -5,6 +5,7 @@ from django.core.files.storage import FileSystemStorage
 from rest_framework.parsers import MultiPartParser, FormParser 
 from rest_framework import permissions
 from drf_spectacular.utils import extend_schema
+from analysis.models import VideoAnalysis
 
 class VideoUploadView(APIView):
     parser_classes = (MultiPartParser, FormParser)
@@ -34,12 +35,20 @@ class VideoUploadView(APIView):
         
         try:
             filename = fs.save(video_file.name, video_file)
-            uploaded_file_url = fs.url(filename)
+            analysis = VideoAnalysis.objects.create(
+                user=request.user,
+                video_name=filename,
+                video_file_path=fs.path(filename),
+                status='pending'
+            )
         except Exception as e:
             return Response({"error": f"Failed to save file: {e}"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
+        # maybe here we should trigger the ML model processing?
+
         return Response({
-            "message": "Video uploaded and saved successfully.",
-            "file_url": uploaded_file_url,
-            "filename": filename
+            "message": "Video uploaded and saved successfully and analysis created.",
+            "filename": filename,
+            "file_path": analysis.video_file_path,
+            "analysis_id": analysis.id,
         }, status=status.HTTP_201_CREATED)
