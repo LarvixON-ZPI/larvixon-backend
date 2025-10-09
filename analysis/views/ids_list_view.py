863c23ace8cd130e7f9208622b55ab343ecb00e8
charmosaa <272682@student.pwr.edu.pl>
2025-10-09 16:45:38 +0200

    feat: ordering for analysis ids list

--- analysis/views/ids_list_view.py
+++ analysis/views/ids_list_view.py
@@ -1,4 +1,4 @@
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, filters
 from django_filters.rest_framework import DjangoFilterBackend
 from analysis.models import VideoAnalysis
 from ..serializers import VideoAnalysisIdSerializer
@@ -7,8 +7,12 @@ from ..filters import VideoAnalysisFilter
 class VideoAnalysisIdListView(generics.ListAPIView):
     serializer_class = VideoAnalysisIdSerializer
     permission_classes = [permissions.IsAuthenticated]
-    filter_backends = [DjangoFilterBackend]
+    filter_backends = [DjangoFilterBackend, filters.OrderingFilter]
     filterset_class = VideoAnalysisFilter
+
+    ordering_fields = ['title', 'created_at', 'completed_at', 'status']
+
+    ordering = ['-created_at'] # default ordering
 
     def get_queryset(self):
         if getattr(self, 'swagger_fake_view', False):
