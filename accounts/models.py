36bd516e4df49aa0e69ca3557a9e1eeab8d2e361
charmosaa <272682@student.pwr.edu.pl>
2025-10-24 17:13:20 +0200

    feat: profile picture field for user

--- accounts/models.py
+++ accounts/models.py
@@ -5,11 +5,14 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import UserManager
 from phonenumber_field.modelfields import PhoneNumberField
+import os
 
 if TYPE_CHECKING:
     from datetime import datetime
     from typing import Any, Dict, Optional, List, Tuple
 
+def user_profile_picture_path(instance, filename):
+    return f'profile_pics/user_{instance.user.id}/{filename}'
 
 class User(AbstractUser):
     """
@@ -54,6 +57,7 @@ class UserProfile(models.Model):
     user: models.OneToOneField[User, User] = models.OneToOneField(
         User, on_delete=models.CASCADE, related_name="profile"
     )
+    profile_picture: models.ImageField = models.ImageField(upload_to=user_profile_picture_path, blank=True, null=True)
     bio: models.TextField = models.TextField(max_length=500, blank=True)
     phone_number: models.CharField = PhoneNumberField(blank=True)
     organization: models.CharField = models.CharField(max_length=255, blank=True)
@@ -62,3 +66,26 @@ class UserProfile(models.Model):
 
     def __str__(self) -> str:
         return f"{self.user.email} - Profile"
+
+    @property
+    def get_profile_picture_url(self):
+        """Returns the URL of the profile picture if it exists."""
+        if self.profile_picture:
+            return self.profile_picture.url
+
+    def save(self, *args, **kwargs):
+        try:
+            this = UserProfile.objects.get(id=self.id)
+            if this.profile_picture != self.profile_picture:
+                if this.profile_picture and os.path.exists(this.profile_picture.path):
+                    this.profile_picture.delete(save=False)
+        except UserProfile.DoesNotExist:
+            pass
+
+        super(UserProfile, self).save(*args, **kwargs)
+
+    def delete(self, *args, **kwargs):
+        if self.profile_picture:
+            self.profile_picture.delete(save=False)
+        super(UserProfile, self).delete(*args, **kwargs)
+
