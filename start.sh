791b0d82108237effa8506385d9836db670227e8
charmosaa <272682@student.pwr.edu.pl>
2025-11-11 17:55:26 +0100

    feat: start.sh update for Azure

--- start.sh
+++ start.sh
@@ -1,10 +1,30 @@
 #!/bin/sh
 
-echo "--- Running migrations ---"
+echo "--- Installing system packages... ---"
+apt-get update
+apt-get install -y --no-install-recommends \
+    postgresql-client \
+    libgl1 \
+    libglib2.0-0 \
+    libsm6 \
+    libxrender1 \
+    libxext6 \
+    libgomp1
+echo "--- System packages installed. ---"
+
+echo "--- Installing pip packages... ---"
+pip install --upgrade pip
+pip install -r requirements.txt
+echo "--- Pip packages installed. ---"
+
+echo "--- Running database migrations ---"
 python manage.py migrate --noinput
 
 echo "--- Collecting static files ---"
 python manage.py collectstatic --noinput
 
-echo "--- Starting supervisord ---"
-exec /usr/bin/supervisord
+echo "--- Starting Celery worker in background... ---"
+celery -A larvixon_site worker -l info &
+
+echo "--- Starting Gunicorn (main process)... ---"
+exec gunicorn larvixon_site.wsgi:application --bind 0.0.0.0:8000 --workers 4
