299f0fe837af9184d9079760344ecc1da17b9ef1
charmosaa <272682@student.pwr.edu.pl>
2025-11-12 22:24:02 +0100

    feat: add logs for pagination

--- larvixon_site/pagination.py
+++ larvixon_site/pagination.py
@@ -1,25 +1,54 @@
 from rest_framework.pagination import PageNumberPagination
 from django.conf import settings
+import sys
 
 
 class ForceHTTPSPaginator(PageNumberPagination):
-    """
-    Custom paginator that forces HTTPS in next and previous URLs
-    if the FORCE_HTTPS setting is enabled.
-    """
 
     def get_paginated_response(self, data):
+
+        # --- START OF DIAGNOSTICS ---
+        print("--- DEBUG: ForceHTTPSPaginator Run ---", file=sys.stderr)
+
+        if settings.FORCE_HTTPS:
+            print(
+                "--- DEBUG: settings.FORCE_HTTPS is True. Starting replacement...",
+                file=sys.stderr,
+            )
+        else:
+            print(
+                "--- DEBUG: settings.FORCE_HTTPS is FALSE. Skipping replacement. ---",
+                file=sys.stderr,
+            )
+        # --- END OF DIAGNOSTICS ---
+
         response = super().get_paginated_response(data)
 
         if settings.FORCE_HTTPS:
             if response.data["next"]:
+                print(
+                    f"--- DEBUG: Original 'next': {response.data['next']}",
+                    file=sys.stderr,
+                )
                 response.data["next"] = response.data["next"].replace(
                     "http://", "https://"
                 )
+                print(
+                    f"--- DEBUG: Corrected 'next': {response.data['next']}",
+                    file=sys.stderr,
+                )
 
             if response.data["previous"]:
+                print(
+                    f"--- DEBUG: Original 'previous': {response.data['previous']}",
+                    file=sys.stderr,
+                )
                 response.data["previous"] = response.data["previous"].replace(
                     "http://", "https://"
+                )
+                print(
+                    f"--- DEBUG: Corrected 'previous': {response.data['previous']}",
+                    file=sys.stderr,
                 )
 
         return response
