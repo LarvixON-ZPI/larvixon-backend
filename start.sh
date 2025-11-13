5b3b1b3f1ff3d388e557c4076ab35ca883399d0b
charmosaa <272682@student.pwr.edu.pl>
2025-11-13 12:23:27 +0100

    feat: add diagnostics to start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
+echo "--- DIAGNOSTIC: Checking Environment Variables ---"
+echo "FORCE_HTTPS = [$FORCE_HTTPS]"
+echo "DEBUG = [$DEBUG]"
+echo "PRE_BUILD_COMMAND (first 30 chars) = [${PRE_BUILD_COMMAND:0:30}...]"
+echo "CELERY_BROKER_URL (first 30 chars) = [${CELERY_BROKER_URL:0:30}...]"
+echo "CELERY_RESULT_BACKEND (first 30 chars) = [${CELERY_RESULT_BACKEND:0:30}...]"
+echo "--- END OF DIAGNOSTICS ---"
+
 echo "--- Running database migrations ---"
 python manage.py migrate --noinput
 
