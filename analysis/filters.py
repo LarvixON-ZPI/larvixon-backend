4d6b3a9892e89aa70f1ec5b2fc8372db7da629f0
charmosaa <272682@student.pwr.edu.pl>
2025-11-18 12:31:27 +0100

    feat: add polish substance name to filters

--- analysis/filters.py
+++ analysis/filters.py
@@ -10,6 +10,12 @@ class VideoAnalysisFilter(django_filters.FilterSet):
         label="Substance Name (English, partial match)",
     )
 
+    substance_name = django_filters.CharFilter(
+        field_name="analysis_results__substance__name_pl",
+        lookup_expr="icontains",
+        label="Substance Name (Polish, partial match)",
+    )
+
     min_confidence = django_filters.NumberFilter(
         field_name="analysis_results__confidence_score",
         lookup_expr="gte",
@@ -25,6 +31,11 @@ class VideoAnalysisFilter(django_filters.FilterSet):
     substance_and_score = django_filters.CharFilter(
         method="filter_by_substance_and_min_score",
         label="Substance Name for Min Score Filter (e.g., cocaine,95.5)",
+    )
+
+    substance_and_score_pl = django_filters.CharFilter(
+        method="filter_by_pl_substance_and_min_score",
+        label="Substance Name (Polish) for Min Score Filter (e.g., kokaina,95.5)",
     )
 
     class Meta:
@@ -51,6 +62,25 @@ class VideoAnalysisFilter(django_filters.FilterSet):
                 "analysis_results__confidence_score",
                 filter=Q(
                     analysis_results__substance__name_en__icontains=substance_name.strip()
+                ),
+            )
+        )
+
+        return annotated_qs.filter(max_confidence_for_substance__gte=min_score)
+
+    def filter_by_pl_substance_and_min_score(self, queryset, name, value):
+        # example: 'kokaina,95.5'
+        try:
+            substance_name, min_score_str = value.split(",")
+            min_score = float(min_score_str)
+        except (ValueError, AttributeError):
+            return queryset
+
+        annotated_qs = queryset.annotate(
+            max_confidence_for_substance=Max(
+                "analysis_results__confidence_score",
+                filter=Q(
+                    analysis_results__substance__name_pl__icontains=substance_name.strip()
                 ),
             )
         )
